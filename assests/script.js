6de8972a901e11306d148f90298dd520a1f9f700
David Galvez <david.dmg17@gmail.com>
2022-05-22 16:00:55 -0700

    continuing the questions const

--- assests/script.js
+++ assests/script.js
@@ -5,3 +5,19 @@ const questions = [
       options: ['string', 'boolean', 'alert', 'numbers'],  
       answer: 3
     },
+    { // question[2]
+        title: 'question 2', // question[2].title
+        options: ['option1', 'option2', 'option3', 'option4'],
+        answer: 2 // question[1].answer
+      },
+      {
+        title: 'question 3',
+        options: ['option1', 'option2', 'option3', 'option4'],
+        answer: 4
+      },
+      {
+        title: 'question 4',
+        options: ['option1', 'option2', 'option3', 'option4'],
+        answer: 4
+      }
+    ];
