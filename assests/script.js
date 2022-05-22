4fcab22ad49ad593de1bc7809c91a60a2d1f70ba
David Galvez <david.dmg17@gmail.com>
2022-05-22 15:58:00 -0700

    linked js and included const

---
+++ assests/script.js
@@ -0,0 +1,7 @@
+/* The questions (data) */
+const questions = [
+    { // question[1]
+      title: 'Commonly used data types DO NOT include?',
+      options: ['string', 'boolean', 'alert', 'numbers'],  
+      answer: 3
+    },
